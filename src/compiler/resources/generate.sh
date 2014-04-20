8502a9c7efd49014881742adb969b161f0422ed0
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-04-19 20:05:09 -0700

    Adding diagnostic generator scripts.

---
+++ src/compiler/resources/generate.sh
@@ -0,0 +1,5 @@
+# Needs diagnosticMessages.json in folder 
+
+node diagnosticCode.generator.js  > diagnosticCode.generated.ts
+
+node diagnosticInformationMap.generator.js > diagnosticInformationMap.generated.ts
