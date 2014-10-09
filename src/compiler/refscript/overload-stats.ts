1a91a00ac2c34bc83a18ee7812ff32b3cf5e1c59
Benjamin Cosman <bcosman@caltech.edu>
2014-10-09 13:06:28 -0700

    Overload stats: now registers interface methods and distinguishes A.foo from B.foo

--- src/compiler/refscript/overload-stats.ts
+++ src/compiler/refscript/overload-stats.ts
@@ -3,7 +3,23 @@
 module TypeScript {
     class OverloadState {
         constructor(public semanticInfoChain: SemanticInfoChain,
-                    public funcs: {[name:string]:number} = {}) { }
+                    public funcs = new Map(),
+                    public overloadFuncs = 0,
+                    public normalFuncs = 0) { }
+    }
+
+    // based on d3's map
+    class Map {
+        private s:{[name:string]:PullTypeSymbol[]};
+        private static PREFIX = "\0"; // prevent collision with built-ins
+        constructor() { this.s = {}; }
+        get(key:string) { return this.s[Map.PREFIX + key]; }
+        set(key:string, value:any) { return this.s[Map.PREFIX + key] = value; }
+    }
+
+    function contains<T>(arr:T[], val:T) {
+        for (var i in arr) { if (arr[i] == val) return true; }
+        return false;
     }
 
     export class OverloadStatGatherer {
@@ -12,31 +28,33 @@ module TypeScript {
                 //case SyntaxKind.ConstructorDeclaration:
                 case SyntaxKind.FunctionDeclaration:
                 case SyntaxKind.MemberFunctionDeclaration:
+                case SyntaxKind.MethodSignature:
+                //case SyntaxKind.CallSignature:
                     var functionDecl = state.semanticInfoChain.getDeclForAST(ast);
                     var name = functionDecl.name;
-                    if (name in state.funcs) break;
+                    console.log(name);
                     var funcSymbol = functionDecl.getSymbol();
+                    var container = funcSymbol.getContainer();
+                    var previouslySeenContainers = state.funcs.get(name);
+                    if (!previouslySeenContainers) previouslySeenContainers = [];
+                    if (contains(previouslySeenContainers, container)) break;
+                    previouslySeenContainers.push(container);
+                    state.funcs.set(name, previouslySeenContainers);
                     var funcTypeSymbol = funcSymbol.type;
-                    var signatures = funcTypeSymbol.getCallSignatures();
-                    state.funcs[name] = signatures.length;
+                    var signatures = funcTypeSymbol.getCallSignatures().length;
+                    signatures > 1 ? state.overloadFuncs++ : state.normalFuncs++;
             }
         }
 
         public static gather(document: Document, semanticInfoChain: SemanticInfoChain) {
             var sourceUnit = document.sourceUnit();
             var state = new OverloadState(semanticInfoChain);
+
             getAstWalkerFactory().simpleWalk(document.sourceUnit(),
                 OverloadStatGatherer.pre, null, state);
 
-            var normalFuncs = 0;
-            var overloadFuncs = 0;
-            for (var func in state.funcs) {
-                if (state.funcs[func] > 1) overloadFuncs ++;
-                else                       normalFuncs++;
-            }
-
-            console.log("Overloaded functions: " + overloadFuncs);
-            console.log("Other functions: " + normalFuncs);
+            console.log("Overloaded functions: " + state.overloadFuncs);
+            console.log("Other functions: " + state.normalFuncs);
         }
     }
 }
